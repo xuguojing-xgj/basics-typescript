139bd6f75ee1209f50a632082bc7e82aae94e2b8
xuguojing-xgj <2313493661@qq.com>
2022-06-12 10:35:40 +0800

    .

---
+++ 01-everyday-types/01-helloWorld.ts
@@ -0,0 +1,3 @@
+let str: string = 'hello, world'
+let num: number = 100
+let bool: boolean = true
---
+++ 01-everyday-types/02-array.ts
@@ -0,0 +1,4 @@
+// 数组 常用写法 type[]  泛用类型 Array<type>
+let list: number[] = [1, 3, 4]
+let list2: Array<string> = ['1', '3F']
+list2 = []
