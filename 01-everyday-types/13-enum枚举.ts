8d3bcc81f6d7079c119eebba11438b86b66421c3
xuguojing-xgj <2313493661@qq.com>
2022-06-12 11:14:31 +0800

    枚举

---
+++ 01-everyday-types/13-enum枚举.ts
@@ -0,0 +1,10 @@
+enum Direaction {
+    Up = 213,
+    Down,
+    Left,
+    Right
+}
+console.log(Direaction.Up);
+console.log(Direaction.Down);
+console.log(Direaction.Left);
+console.log(Direaction.Right);
