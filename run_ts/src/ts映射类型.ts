6a3c7b60fde4083f0c7711193f038ee9b0b1fad1
xuguojing-xgj <2313493661@qq.com>
2022-08-18 21:41:08 +0800

    ts映射类型 and 源码文件

--- run_ts/src/ts映射类型.ts
+++ run_ts/src/ts映射类型.ts
@@ -1,4 +1,20 @@
-//
-export default {
-    
+// typescript内置了Readonly、Partial、Pick、Record、Required五个高级类型
+// Readonly只读 举例
+interface Person {
+    name: string;
+    age?: number;
 }
+
+// Readonly只读 源码
+/**
+ * type Readonly<T> = {
+    readonly [P in keyof T]: T[P];
+   };
+ */
+// 例子 : 
+type newPerson = Readonly<Person>
+
+// newPerson === {
+//     Readonly name: string,
+//     Readonly age?: number
+// }
