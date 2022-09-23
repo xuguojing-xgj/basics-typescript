4e3c2cc8adb2572d3bb89b4ef80389c0cbe47676
Violet 02 <79581129+xuguojing-xgj@users.noreply.github.com>
2022-09-24 00:00:39 +0800

    Update 16-条件类型.ts

--- 02-Types-TypeScript/16-条件类型.ts
+++ 02-Types-TypeScript/16-条件类型.ts
@@ -1,3 +1,13 @@
 /**
-* 
+*  语法中 extends 是关键字
+*  T U X Y 均表示一种类型
+*  T 可以 赋值 给 U  条件结果为true 类型为X 否则 条件结果为false 类型为 Y 
 */
+
+T extends U ? X : Y
+
+// number
+type T0 = number extends boolean ? string : number
+
+// string
+type T1 = true extends boolean ? string : number
