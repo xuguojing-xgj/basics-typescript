c777f6f852930a0cecf0013586c3df9aa86ff795
xuguojing-xgj <2313493661@qq.com>
2022-11-26 20:55:27 +0800

    update files

--- Run-TypeScript/src/04-内置类型工具.ts
+++ Run-TypeScript/src/04-内置类型工具.ts
@@ -6,4 +6,16 @@ interface A {
 }
 type T = Partial<A> // {x?: number, y?: number}
 
-const _a : T = { x: 0 }
+const _a: T = { x: 0 }
+
+
+// Required<T> 该工具能够构造一个新类型, 并将实际类型参数T中的所有属性变为必选属性
+
+interface B {
+    name?: string,
+    age?: number,
+}
+
+type Q = Required<B> // {name: string, age: number}
+
+const _b: Q = { name: '小明', age: 18 }
