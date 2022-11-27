aa072be7c6ee0d74aac9db845479ae0d0754de79
xuguojing-xgj <2313493661@qq.com>
2022-11-27 22:20:47 +0800

    update files

--- Run-TypeScript/src/04-内置类型工具.ts
+++ Run-TypeScript/src/04-内置类型工具.ts
@@ -19,3 +19,15 @@ interface B {
 type Q = Required<B> // {name: string, age: number}
 
 const _b: Q = { name: '小明', age: 18 }
+
+// Readonly<T>  该工具能够构造一个新类型, 并将实际类型参数T中的所有属性变为只读属性
+
+interface C {
+    name: string,
+    age: number,
+}
+
+type W = Readonly<C>
+
+const _c: W = { name: '小明', age: 18 }
+_c.name = '小红'
