9f368ac81e927b603e4ed17064dab48ee96a6a6e
xuguojing-xgj <2313493661@qq.com>
2022-11-30 23:22:05 +0800

    update files

--- Run-TypeScript/src/05-泛型类型别名.ts
+++ Run-TypeScript/src/05-泛型类型别名.ts
@@ -64,14 +64,30 @@ identity({ x: 0, y: 0 })
 // 在该语法中, 默认类型位于泛型约束之后, 类型参数T 的泛型约束为number类型, 默认类型为数字字面类型的联合类型
 function _fn<T extends number = 0 | 1>(num: T): T {
     return num
- }
+}
 //  <T extends number = 0 | 1> 约束后的泛型 T DefaultType 为 number 类型的 0 | 1
 _fn<100>(100)
 
 // 如果泛型形式类型参数定义了泛型约束, 那么函数传入的实际类型参数必须符合泛型约束, 否则将抛出错误
 
-function _p<T extends boolean>() {}
+function _p<T extends boolean>() { }
 _p<true>()
 _p<false>()
 _p<boolean>()
 // _p<string>() ->  类型“string”不满足约束“boolean”
+
+/**
+ * 泛型约束引用类型参数
+ */
+// 在泛型约束中,约束类型允许引用当前形式类型参数列表中的其他类型参数,
+// 形式类型参数 U 引用了在其左侧定义的形式类型参数 T 作为约束类型
+// <T , U extends T>
+
+// 形式类型参数 T 引用了在其右侧定义的形式类型参数 U
+// <T extends U, U>
+
+// 需要注意的是,一个形式类型参数不允许直接或间接的将其自身作为约束类型,
+// 否侧将产生引用的编译错误
+// <T extends T>  // 错误
+// <T extends U, U extends T>  // 错误
+
